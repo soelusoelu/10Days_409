b56a6ac10cc5a74b73ff952d7bdcea92678b573c
takano <llmn.0419@gmail.com>
2021-09-15 16:38:35 +0900

    連続ショットと弾消滅処理を追加

--- 10Days_409/Assets/Scripts/Bullet/Bullet.cs
+++ 10Days_409/Assets/Scripts/Bullet/Bullet.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 120f;
+    [SerializeField] private float destroyTime = 5f;
+    private Timer timer;
+
+    void Start() {
+        timer = new Timer();
+        timer.SetLimitTime(destroyTime);
+    }
 
     void Update() {
         transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+
+        timer.Update();
+        if (timer.IsTime()) {
+            Destroy(gameObject);
+        }
     }
 }
--- 10Days_409/Assets/Scripts/Player/BulletShooter.cs
+++ 10Days_409/Assets/Scripts/Player/BulletShooter.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class BulletShooter : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
+    [SerializeField] private float reshotInterval = 0.25f;
+    private Timer timer;
+
+    void Start() {
+        timer = new Timer();
+        timer.SetLimitTime(reshotInterval);
+        timer.Overlimit();
+    }
 
     void Update() {
-        if (!Input.GetKeyDown(KeyCode.Space)) {
+        timer.Update();
+
+        if (!Input.GetKey(KeyCode.Space)) {
             return;
         }
+        if (!timer.IsTime()) {
+            return;
+        }
+
+        timer.Reset();
 
         var newBullet = Instantiate(bullet);
         newBullet.transform.position = transform.position;
