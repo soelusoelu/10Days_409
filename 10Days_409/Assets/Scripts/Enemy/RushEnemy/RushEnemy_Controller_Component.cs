3bfbea80cddf5187b1df521804d693034f3493d4
bearbeatcat <chee-volcano@nys-dreamnet.ne.jp>
2021-09-16 11:32:14 +0900

    [update] RushEnemyのLevel1の動きと表情の実装

--- 10Days_409/Assets/Scripts/Enemy/RushEnemy/RushEnemy_Controller_Component.cs
+++ 10Days_409/Assets/Scripts/Enemy/RushEnemy/RushEnemy_Controller_Component.cs
@@ -8,6 +8,7 @@ public class RushEnemy_Controller_Component : MonoBehaviour
     private Level _mLevel;
     private RushEnemy_Level3_Move_Component _mLevel3MoveComponent = null;
     private RushEnemy_Level2_Move_Component _mLevel2MoveComponent = null;
+    private RushEnemy_Level1_Move_Component _mLevel1MoveComponent = null;
 
     [SerializeField] private GameObject _mLevel2Body;
     [SerializeField] private GameObject _mLevel3Body;
@@ -59,6 +60,13 @@ public class RushEnemy_Controller_Component : MonoBehaviour
             _mLevel2MoveComponent = null;
         }
 
+
+        if (_mLevel1MoveComponent != null)
+        {
+            Destroy(this.GetComponent<RushEnemy_Level1_Move_Component>());
+            _mLevel1MoveComponent = null;
+        }
+
         if (currentLevel == 3)
         {
             _mLevel3MoveComponent = gameObject.AddComponent<RushEnemy_Level3_Move_Component>();
@@ -69,6 +77,11 @@ public class RushEnemy_Controller_Component : MonoBehaviour
             _mLevel2MoveComponent = gameObject.AddComponent<RushEnemy_Level2_Move_Component>();
             ChangeLevel2();
         }
+        else
+        {
+            _mLevel1MoveComponent = gameObject.AddComponent<RushEnemy_Level1_Move_Component>();
+            ChangeLevel1();
+        }
     }
 
     void ChangeLevel2()
@@ -78,6 +91,7 @@ public class RushEnemy_Controller_Component : MonoBehaviour
 
         _mFaceAnimator.SetLayerWeight(1, 0.0f);
         _mFaceAnimator.SetLayerWeight(2, 1.0f);
+        _mFaceAnimator.SetLayerWeight(3, 0.0f);
     }
 
     void ChangeLevel3()
@@ -87,7 +101,18 @@ public class RushEnemy_Controller_Component : MonoBehaviour
 
         _mFaceAnimator.SetLayerWeight(1, 1.0f);
         _mFaceAnimator.SetLayerWeight(2, 0.0f);
+        _mFaceAnimator.SetLayerWeight(3, 0.0f);
 
+    }
+
+    void ChangeLevel1()
+    {
+        _mLevel3Body.SetActive(false);
+        _mLevel2Body.SetActive(false);
+
+        _mFaceAnimator.SetLayerWeight(1, 0.0f);
+        _mFaceAnimator.SetLayerWeight(2, 0.0f);
+        _mFaceAnimator.SetLayerWeight(3, 1.0f);
     }
 
 }
---
+++ 10Days_409/Assets/Scripts/Enemy/RushEnemy/RushEnemy_Level1_Move_Component.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RushEnemy_Level1_Move_Component : MonoBehaviour
+{
+    [SerializeField] private float _mMoveSpeed = 0.02f;
+    [SerializeField] private float _mRotateTime = 3.0f;
+
+    private Timer _mTimer;
+
+    [SerializeField] private Vector3 _mMoveTargetAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mMoveTargetAngle = transform.localEulerAngles;
+
+        _mTimer = new Timer();
+        _mTimer.SetLimitTime(_mRotateTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mTimer.Update();
+
+        if (_mTimer.IsTime())
+        {
+            _mTimer.Reset();
+            ChangeRotate();
+            return;
+        }
+
+        RotateUpdate();
+        Move();
+    }
+
+    void Move()
+    {
+        var vec = -transform.forward;
+
+        var currentPos = transform.position;
+
+        currentPos += vec * _mMoveSpeed;
+
+        transform.position = currentPos;
+    }
+
+    void RotateUpdate()
+    {
+        transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, _mMoveTargetAngle, Time.time);
+    }
+
+    void ChangeRotate()
+    {
+        var currentRotation = transform.localEulerAngles;
+
+        _mMoveTargetAngle = new Vector3(currentRotation.x, Random.Range(-120.0f, 120.0f),
+            currentRotation.z);
+    }
+}
