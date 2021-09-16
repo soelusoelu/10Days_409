5ab941fba2de8e9831ec4a95379409d5e85c6e3e
TakanoKakeru <llmn.0419@gmail.com>
2021-09-16 13:08:24 +0900

    爆発オブジェクト削除

--- 10Days_409/Assets/Scripts/Player/Bullet/ExplodeCollider.cs
+++ 10Days_409/Assets/Scripts/Player/Bullet/ExplodeCollider.cs
@@ -6,9 +6,18 @@ public class ExplodeCollider : MonoBehaviour
 {
     [SerializeField] private float explodeRange = 6f;
     private SphereCollider sphere = null;
+    private bool waitOneFrame = false;
 
     private void Start() {
         sphere = GetComponent<SphereCollider>();
         sphere.radius = explodeRange / 2f;
+    }
+
+    private void Update() {
+        if (waitOneFrame) {
+            Destroy(gameObject);
+        }
+
+        waitOneFrame = true;
     }
 }
