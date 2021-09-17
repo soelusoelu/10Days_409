68e64b167790589a406437946e6c7f607e06ab95
takano <llmn.0419@gmail.com>
2021-09-17 14:47:03 +0900

    多分バグ治った

---
+++ 10Days_409/Assets/Scripts/Wave/WaveSystem.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waves;
+    private EnemyCreater enemyCreater;
+    private int currentIndex = 0;
+    public delegate void CallbackOnEndWave();
+    private CallbackOnEndWave onEndWave;
+    private CallbackOnEndWave onAllEndWave;
+
+    private void Start() {
+        var newWave = waves[currentIndex];
+
+        if (waves.Length > 0) {
+            var newCreater = Instantiate(newWave);
+            enemyCreater = newCreater.GetComponent<EnemyCreater>();
+        }
+    }
+
+    private void Update() {
+        if (!enemyCreater.CreatedLastEnemy()) {
+            return;
+        }
+
+        if (enemyCreater.IsDestroyedEnemys()) {
+            if (currentIndex == waves.Length) {
+                return;
+            }
+
+            Debug.Log("enemys destroyed.");
+
+            onEndWave?.Invoke();
+
+            ChangeWave();
+        }
+    }
+
+    public int GetCurrentWave() {
+        return currentIndex;
+    }
+
+    public void OnEndWave(CallbackOnEndWave f) {
+        onEndWave += f;
+    }
+
+    public void OnAllEndWave(CallbackOnEndWave f) {
+        onAllEndWave += f;
+    }
+
+    private void ChangeWave() {
+        ++currentIndex;
+        if (currentIndex >= waves.Length) {
+            Debug.Log("ended all waves.");
+            onAllEndWave?.Invoke();
+        } else {
+            var newCreater = Instantiate(waves[currentIndex]);
+            enemyCreater = newCreater.GetComponent<EnemyCreater>();
+        }
+    }
+}
